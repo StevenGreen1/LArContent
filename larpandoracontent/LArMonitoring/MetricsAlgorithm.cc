8576bc797b6ae237317aa35994a6861726620da0
Steven Green <sg1sg2sg3@hotmail.co.uk>
2018-01-16 14:03:51 +0000

    Compiler bug fix 4.

--- larpandoracontent/LArMonitoring/MetricsAlgorithm.cc
+++ larpandoracontent/LArMonitoring/MetricsAlgorithm.cc
@@ -39,14 +39,10 @@ StatusCode MetricsAlgorithm::Run()
 {
 //    PANDORA_MONITORING_API(SetTreeVariable(this->GetPandora(), m_treeName.c_str(), "minorAxisTwoEigenvalueBeamSlice", pSlicePropertiesBeam->m_minorAxisTwoEigenvalue));
 
-    const PfoList *pPfoList(nullptr);
-    PANDORA_THROW_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_INITIALIZED, !=, PandoraContentApi::GetList(*this, m_pfoListName, pPfoList));
-
     const MCParticleList *pMCParticleList(nullptr);
     PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::GetList(*this, m_mcParticleListName, pMCParticleList));
 
-    std::cout << "pPfoList->size() " << pPfoList->size() << std::endl;
-    std::cout << "pMCParticleList->size() " << pMCParticleList->size() << std::endl;
+    IntVector triggeredParticleID;
 
     for (const MCParticle *pMCParticle : *pMCParticleList)
     {
@@ -54,9 +50,37 @@ StatusCode MetricsAlgorithm::Run()
 
         if (2001 == pLArMCParticle->GetNuanceCode())
         {
+            triggeredParticleID.push_back(pLArMCParticle->GetParticleId());
             std::cout << "Found target beam particle " << pLArMCParticle->GetParticleId() << std::endl;
         }
     }
+
+    const PfoList *pPfoList(nullptr);
+    PANDORA_THROW_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_INITIALIZED, !=, PandoraContentApi::GetList(*this, m_pfoListName, pPfoList));
+
+    int nTrackLike(0);
+    int nShowerLike(0);
+
+    for (const Pfo *const pPfo : *pPfoList)
+    {
+        if (12 == std::abs(pPfo->GetParticleId()))
+        {
+            nShowerLike++;
+            std::cout << "Shower Beam Candidate Identified" << std::endl;
+        }
+        else if (14 == std::abs(pPfo->GetParticleId()))
+        {
+            nTrackLike++;
+            std::cout << "Track Beam Candidate Identified" << std::endl;
+        }
+    }
+
+    int nTriggeredParticles(triggeredParticleID.size());
+
+    PANDORA_MONITORING_API(SetTreeVariable(this->GetPandora(), m_treeName.c_str(), "TriggeredParticleID", triggeredParticleID));
+    PANDORA_MONITORING_API(SetTreeVariable(this->GetPandora(), m_treeName.c_str(), "nTriggeredParticles", nTriggeredParticles));
+    PANDORA_MONITORING_API(SetTreeVariable(this->GetPandora(), m_treeName.c_str(), "nShowerLikePFOs", nShowerLike));
+    PANDORA_MONITORING_API(SetTreeVariable(this->GetPandora(), m_treeName.c_str(), "nTrackLikePFOs", nTrackLike));
 
     return STATUS_CODE_SUCCESS;
 }
