e41297bfea7877ff8e40ab99d70bb3c887a572d1
Steven Green <sg1sg2sg3@hotmail.co.uk>
2019-02-20 10:14:17 +0000

    Adding the LArSliceAnalysis class.

---
+++ larpandoracontent/LArDeepLearning/LArSliceAnalysis.cc
@@ -0,0 +1,26 @@
+/**
+ *  @file   larpandoracontent/LArDeepLearning/LArSliceAnalysis.cc
+ *
+ *  @brief  Implementation of the lar slice analysis class.
+ *
+ *  $Log: $
+ */
+
+#include "larpandoracontent/LArDeepLearning/LArSliceAnalysis.h"
+
+using namespace pandora;
+
+namespace lar_content
+{
+
+SliceAnalysis::SliceAnalysis()
+{
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+SliceAnalysis::~SliceAnalysis()
+{
+}
+
+} // namespace lar_content
---
+++ larpandoracontent/LArDeepLearning/LArSliceAnalysis.h
@@ -0,0 +1,38 @@
+/**
+ *  @file   larpandoracontent/LArDeepLearning/LArSliceAnalysis.h
+ *
+ *  @brief  Header file for the lar slice analysis class.
+ *
+ *  $Log: $
+ */
+
+#ifndef LAR_SLICE_ANALYSIS_H
+#define LAR_SLICE_ANALYSIS_H 1
+
+#include "larpandoracontent/LArDeepLearning/LArSliceAnalysis.h"
+
+#include "Pandora/StatusCodes.h"
+
+namespace lar_content
+{
+
+/**
+ *  @brief  SliceAnalysis class
+ */
+class SliceAnalysis
+{
+public:
+    /**
+     *  @brief  Constructor
+     */
+    SliceAnalysis();
+
+    /**
+     *  @brief  Destructor
+     */
+    ~SliceAnalysis();
+};
+
+} // namespace lar_content
+
+#endif // #ifndef LAR_SLICE_ANALYSIS_H
