1cd821f59706312a50bcd0965d802494d08a19ff
Steven Green <sg1sg2sg3@hotmail.co.uk>
2019-08-14 13:50:48 +0100

    Adding deep learning cluster and pfo characterisation algorithms.

---
+++ larpandoracontent/LArTrackShowerId/DLClusterCharacterisationAlgorithm.cc
@@ -0,0 +1,56 @@
+/**
+ *  @file   larpandoracontent/LArTrackShowerId/DLClusterCharacterisationAlgorithm.cc
+ *
+ *  @brief  Implementation of the deep learning based cluster characterisation algorithm class.
+ *
+ *  $Log: $
+ */
+
+#include "Pandora/AlgorithmHeaders.h"
+
+#include "larpandoracontent/LArTrackShowerId/DLClusterCharacterisationAlgorithm.h"
+
+using namespace pandora;
+
+namespace lar_content
+{
+
+bool DLClusterCharacterisationAlgorithm::IsClearTrack(const Cluster *const pCluster) const
+{
+    const OrderedCaloHitList &orderedCaloHitList(pCluster->GetOrderedCaloHitList());
+
+    int nShowerHits(0), nTrackHits(0);
+
+    for (OrderedCaloHitList::const_iterator iter = orderedCaloHitList.begin(); iter != orderedCaloHitList.end(); ++iter)
+    {
+        for (CaloHitList::const_iterator hitIter = iter->second->begin(), hitIterEnd = iter->second->end(); hitIter != hitIterEnd; ++hitIter)
+        {
+            const CaloHit *const pCaloHit = *hitIter;
+            const PropertiesMap &properties(pCaloHit->GetPropertiesMap());
+            const bool isTrack(properties.find("IsTrack") == properties.end() ? false : true);
+
+            if (isTrack)
+            {
+                nTrackHits++;
+            }
+            else
+            {
+                nShowerHits++;
+            }
+        }
+    }
+
+    if (nTrackHits >= nShowerHits)
+        return true;
+
+    return false;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+StatusCode DLClusterCharacterisationAlgorithm::ReadSettings(const TiXmlHandle xmlHandle)
+{
+    return ClusterCharacterisationBaseAlgorithm::ReadSettings(xmlHandle);
+}
+
+} // namespace lar_content
---
+++ larpandoracontent/LArTrackShowerId/DLPfoCharacterisationAlgorithm.cc
@@ -0,0 +1,63 @@
+/**
+ *  @file   larpandoracontent/LArTrackShowerId/DLPfoCharacterisationAlgorithm.cc
+ *
+ *  @brief  Implementation of the deep learning based pfo characterisation algorithm class.
+ *
+ *  $Log: $
+ */
+
+#include "Pandora/AlgorithmHeaders.h"
+
+#include "larpandoracontent/LArTrackShowerId/DLPfoCharacterisationAlgorithm.h"
+
+using namespace pandora;
+
+namespace lar_content
+{
+
+bool DLPfoCharacterisationAlgorithm::IsClearTrack(const Cluster *const pCluster) const
+{
+    const OrderedCaloHitList &orderedCaloHitList(pCluster->GetOrderedCaloHitList());
+
+    int nShowerHits(0), nTrackHits(0);
+
+    for (OrderedCaloHitList::const_iterator iter = orderedCaloHitList.begin(); iter != orderedCaloHitList.end(); ++iter)
+    {
+        for (CaloHitList::const_iterator hitIter = iter->second->begin(), hitIterEnd = iter->second->end(); hitIter != hitIterEnd; ++hitIter)
+        {
+            const CaloHit *const pCaloHit = *hitIter;
+            const PropertiesMap &properties(pCaloHit->GetPropertiesMap());
+            const bool isTrack(properties.find("IsTrack") == properties.end() ? false : true);
+
+            if (isTrack)
+            {
+                nTrackHits++;
+            }
+            else
+            {
+                nShowerHits++;
+            }
+        }
+    }
+
+    if (nTrackHits >= nShowerHits)
+        return true;
+
+    return false;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+bool DLPfoCharacterisationAlgorithm::IsClearTrack(const pandora::ParticleFlowObject *const /*pPfo*/) const
+{
+    throw StatusCodeException(STATUS_CODE_NOT_ALLOWED);
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+StatusCode DLPfoCharacterisationAlgorithm::ReadSettings(const TiXmlHandle xmlHandle)
+{
+    return PfoCharacterisationBaseAlgorithm::ReadSettings(xmlHandle);
+}
+
+} // namespace lar_content
